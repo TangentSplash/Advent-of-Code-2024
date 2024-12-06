8b5f9d83ef6bb105baedc6081f250bc69cb8d377
TangentSplash <68692742+TangentSplash@users.noreply.github.com>
2024-12-06 00:10:35 +0000

    Day 5 Part 2

--- Day 5/OrderClass.py
+++ Day 5/OrderClass.py
@@ -2,12 +2,19 @@ class OrderClass:
     def __init__(self,number,before):
         self.number = number
         self.numsBefore = [before]
+        self.latestIndex = -1
     
     def checkShouldBeBefore(self,listOfAfter):
         for after in listOfAfter:
             if (after in self.numsBefore):
+                self.latestIndex = listOfAfter.index(after)
                 return False
         return True
     
     def append(self, num):
         self.numsBefore.append(num)
+        
+    def reorderSubList(self,list,i):
+        num = list.pop(i+1+self.latestIndex)
+        list.insert(i,num)
+        return list
--- Day 5/Part2.py
+++ Day 5/Part2.py
@@ -27,17 +27,22 @@ for line in Input:
         createOrder(line)
     else:
         valid = True
+        wasOrdered = True
         sequenceString = line.split(',')
         sequence = [int(item) for item in sequenceString]
-        for number in sequence:
+        sequence_copy = sequence.copy()
+        for number in sequence_copy:
             if number in rules:
                 rule = rules[number]
-                i = sequence.index(number)
-                if(not rule.checkShouldBeBefore(sequence[i+1:])):
-                    valid = False
-                    break
-        if(not valid):
-            
+                valid = False
+                while (not valid):
+                    i = sequence.index(number)
+                    valid = True
+                    if(not rule.checkShouldBeBefore(sequence[i+1:])):
+                        wasOrdered = False
+                        valid = False
+                        rule.reorderSubList(sequence,i)
+        if(not wasOrdered):
             sum += sequence[floor(len(sequence) / 2)]
                     
 print("The sum is " + str(sum))
