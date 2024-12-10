3e280a1c6c5a02911a957f905b4c6cfdbd0f26ae
TangentSplash <68692742+TangentSplash@users.noreply.github.com>
2024-12-10 21:42:46 +0000

    Day 7 Part 2

---
+++ Day 7/Part2.py
@@ -0,0 +1,46 @@
+INPUT ='Day 7/input.txt'
+Fileinput = open(INPUT,"r")
+Input = Fileinput.readlines()
+
+ADD = 0
+MULT = 1
+CONCAT = 2
+    
+def tryOperation(target, sum,operands, operation):
+    thisOperand = operands.pop(0)
+    match operation:
+        case 0: # Add
+            sum = sum + thisOperand
+        case 1: # Mult
+            sum = sum * thisOperand
+        case 2: # Concat
+            sum = int(str(sum)+str(thisOperand))
+        case _:
+            print('Error, invalid operation')
+    if (sum > target):
+        return False
+    elif (len(operands) == 0):
+        if (sum == target):
+            return True
+        else:
+            return False
+    else:
+        return tryOperations(target, sum, operands)
+    
+def tryOperations(target, sum, operands):
+    if (tryOperation(target, sum, operands.copy(), ADD)):
+            return True
+    elif (tryOperation(target, sum, operands.copy(), MULT)):
+        return True
+    else:
+        return tryOperation(target, sum, operands.copy(), CONCAT)
+    
+totalCalibration = 0
+for line in Input:
+    vals = line.split(': ')
+    target = int(vals[0])
+    operands = [int(i) for i in vals[1].split(' ')]
+    if(tryOperations(target,operands[0],operands[1:])):
+        totalCalibration += target
+
+print('The total calibration is ' + str(totalCalibration))
